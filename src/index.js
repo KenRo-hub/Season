bed9c6c72285589b4f52d79d818468c90180545e
Edgar Kenyi Rojo Moreno <edgar.kenyi.rojo@gmail.com>
2020-07-24 18:05:28 -0500

    primer commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+
+const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (err) => console.log(err)
+  );
+
+  return <div>hi</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
